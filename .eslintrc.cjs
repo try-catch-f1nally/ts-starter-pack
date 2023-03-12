d58042ef64bc48850b86ae1a8158b1b8c11fb034
try-catch-f1nally <finally.catch.try@gmail.com>
2023-03-12 21:22:13 +0200

    Disable a few eslint rules for typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,11 @@ module.exports = {
   rules: {
     'no-empty': ['off'],
     'sonarjs/no-ignored-return': ['off'],
-    '@typescript-eslint/no-misused-promises': ['error', {checksVoidReturn: false}]
+    '@typescript-eslint/no-misused-promises': ['error', {checksVoidReturn: false}],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-non-null-assertion': ['off'],
+    '@typescript-eslint/no-unsafe-call': ['off'],
+    '@typescript-eslint/no-unsafe-assignment': ['off'],
+    '@typescript-eslint/no-unsafe-member-access': ['off']
   }
 };
